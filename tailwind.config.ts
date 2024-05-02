a34baa78071144d2c6910fe606659331af1ab082
Julien Viala <julien.viala@shine.fr>
2024-05-02 21:06:07 +0200

    tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,14 @@ export default {
       sans: ["DM Sans Variable", "sans-serif"],
       arial: ["Arial", "sans-serif"],
     },
+    colors: {
+      primary: "#0f2e20",
+      paragraph: "#05100b",
+      secondary: "#3d4c5e",
+      neutral1: "#fefaf5",
+      neutral2: "#f4efea",
+      neutral3: "#d9e1dd",
+    },
   },
   plugins: [],
 } satisfies Config;
