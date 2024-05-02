05fc246e55396a9988a4764f17d573d2b060abfd
Julien Viala <julien.viala@shine.fr>
2024-05-02 15:23:35 +0200

    test tailwind custom font size

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,29 @@
+import { type Config } from "tailwindcss";
+
+const BASE = 18;
+const rem = (px: number) => `${px / BASE}rem`;
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-};
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    fontSize: {
+      base: `${BASE}px`,
+      "body-small": [
+        rem(15),
+        {
+          lineHeight: "140%",
+          fontWeight: 400,
+        },
+      ],
+      "body-normal": [
+        rem(18),
+        {
+          lineHeight: "140%",
+          fontWeight: 400,
+        },
+      ],
+    },
+  },
+  plugins: [],
+} satisfies Config;
