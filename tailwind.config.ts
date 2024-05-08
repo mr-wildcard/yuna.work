1af4dee2129f2c787d119c8bb46b031872855ee7
Julien Viala <julien.viala@shine.fr>
2024-05-08 21:06:38 +0200

    chore(tailwind): remove font size base

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,5 @@
 import { type Config } from "tailwindcss";
 
-const BASE = 18;
-const rem = (px: number) => `${px / BASE}rem`;
-
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -10,9 +7,6 @@ export default {
       screens: {
         "2xl": "1440px",
       },
-    },
-    fontSize: {
-      base: `${BASE}px`,
     },
     fontFamily: {
       sans: ["DM Sans Variable", "sans-serif"],
