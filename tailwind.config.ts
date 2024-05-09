99b8e3c06e9fb050ba67d7dcc24997073f4c6e84
Julien Viala <julien.viala@shine.fr>
2024-05-09 23:19:49 +0200

    refactor(tailwind): font family in config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,134 @@ export default {
       sans: ["DM Sans Variable", "sans-serif"],
       arial: ["Arial", "sans-serif"],
     },
+    fontSize: {
+      "display-large-highlight": [
+        "36px",
+        {
+          lineHeight: "120%",
+          fontWeight: "500",
+        },
+      ],
+      "display-large": [
+        "36px",
+        {
+          lineHeight: "120%",
+          fontWeight: "400",
+        },
+      ],
+      "display-medium": [
+        "32px",
+        {
+          lineHeight: "120%",
+          fontWeight: "400",
+        },
+      ],
+      "display-small": [
+        "30px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "headline-small": [
+        "24px",
+        {
+          lineHeight: "120%",
+          fontWeight: "400",
+        },
+      ],
+      "headline-small-highlight": [
+        "22px",
+        {
+          lineHeight: "120%",
+          fontWeight: "700",
+        },
+      ],
+      "headline-large": [
+        "26px",
+        {
+          lineHeight: "120%",
+          fontWeight: "400",
+        },
+      ],
+      "headline-large-highlight": [
+        "24px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "title-small": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "title-normal": [
+        "20px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "title-large": [
+        "20px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "body-small": [
+        "15px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "body-normal": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "body-normal-highlight": [
+        "18px",
+        {
+          lineHeight: "160%",
+          fontWeight: "500",
+        },
+      ],
+      "tag-label-highlight": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+      "tag-label": [
+        "16px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+      "menu-label-highlight": [
+        "18px",
+        {
+          lineHeight: "normal",
+          fontWeight: "700",
+        },
+      ],
+      "menu-label": [
+        "18px",
+        {
+          lineHeight: "140%",
+          fontWeight: "400",
+        },
+      ],
+    },
     colors: {
       primary: "#0f2e20",
       paragraph: "#05100b",
