930c7882fcebc9e17161c00f31458174833d2618
Julien Viala <julien.viala@shine.fr>
2024-09-03 21:41:51 +0200

    chore: remove e2e tests

--- tests/homepage.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("Snapshot homepage", async ({ page }) => {
-  await page.goto("/");
-
-  await page.waitForFunction(() => document.fonts.ready);
-
-  await expect(page.getByRole("list")).toHaveScreenshot();
-});
