d699c1dada1e3c083aae253f82655de5c28b64bd
Julien Viala <julien.viala@shine.fr>
2025-03-21 20:42:54 +0100

    feat: close mobile menu with escape

--- src/components/Header/scripts/index.ts
+++ src/components/Header/scripts/index.ts
@@ -12,18 +12,40 @@ const mobileMenuOpener = document.querySelector<HTMLElement>(
 
 const mobileMenu = document.querySelector<HTMLElement>("#mobile-menu");
 
+function openMobileMenu() {
+  if (header && mobileMenuOpener && mobileMenu) {
+    header.setAttribute("data-menu-opened", "true");
+    mobileMenuOpener.setAttribute("aria-expanded", "true");
+    document.body.classList.add("overflow-hidden");
+  }
+}
+
+function closeMobileMenu() {
+  if (header && mobileMenuOpener && mobileMenu) {
+    header.setAttribute("data-menu-opened", "false");
+    mobileMenuOpener.setAttribute("aria-expanded", "false");
+    document.body.classList.remove("overflow-hidden");
+  }
+}
+
+function onKeypress(event: KeyboardEvent) {
+  if (event.key === "Escape") {
+    closeMobileMenu();
+  }
+}
+
 if (header && mobileMenuOpener && mobileMenu) {
   mobileMenuOpener.addEventListener("click", () => {
     const { menuOpened } = header.dataset;
 
     if (menuOpened === "true") {
-      header.setAttribute("data-menu-opened", "false");
-      mobileMenuOpener.setAttribute("aria-expanded", "false");
-      document.body.classList.remove("overflow-hidden");
+      document.body.addEventListener("keydown", onKeypress);
+
+      closeMobileMenu();
     } else {
-      header.setAttribute("data-menu-opened", "true");
-      mobileMenuOpener.setAttribute("aria-expanded", "true");
-      document.body.classList.add("overflow-hidden");
+      document.body.addEventListener("keydown", onKeypress);
+
+      openMobileMenu();
     }
   });
 }
