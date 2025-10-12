c2a93aaf50f3436dde2d0cd6e1b82d02a9a52d28
Julien Viala <julien.viala@shine.fr>
2025-10-12 22:01:39 +0200

    Revert "chore(ci): use branch name as deployment url"
    This reverts commit a757f93256fcd94738b1ad8cb9a1c364eef8eafd.

--- astro.config.ts
+++ astro.config.ts
@@ -11,10 +11,12 @@ const { ASTRO_DEV_SERVER_LOCAL_HOST, ASTRO_DEV_SERVER_LOCAL_PORT } = loadEnv(
 );
 
 function getSiteURL() {
-  if (process.env.CF_PAGES_BRANCH === "master") {
-    return "https://yuna.work/";
-  } else if (process.env.CI === "true") {
-    return `https://${process.env.GITHUB_HEAD_REF}.yuna-work.pages.dev/`;
+  if (process.env.CF_PAGES) {
+    if (process.env.CF_PAGES_BRANCH === "master") {
+      return "https://yuna.work/";
+    } else {
+      return process.env.CF_PAGES_URL;
+    }
   } else {
     return `http://${ASTRO_DEV_SERVER_LOCAL_HOST}:${ASTRO_DEV_SERVER_LOCAL_PORT}/`;
   }
