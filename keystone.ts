ef70239761357ca899f01fb2e2b263cda9815020
Julien Viala <julien.viala@shine.fr>
2025-10-22 15:26:24 +0200

    feat(cms): dummy list

--- keystone.ts
+++ keystone.ts
@@ -1,8 +1,19 @@
-import { config } from "@keystone-6/core";
+import { config, list } from "@keystone-6/core";
+import { allowAll } from "@keystone-6/core/access";
+import { text } from "@keystone-6/core/fields";
 
 export default config({
   db: {
     provider: "postgresql",
     url: "",
+  },
+  lists: {
+    User: list({
+      access: allowAll,
+      fields: {
+        name: text({ validation: { isRequired: true } }),
+        email: text({ validation: { isRequired: true }, isIndexed: "unique" }),
+      },
+    }),
   },
 });
